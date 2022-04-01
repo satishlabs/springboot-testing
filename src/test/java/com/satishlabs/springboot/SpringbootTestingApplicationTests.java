5b86ccd63e53a82af34fc3d73c8c864df9d92637
Satish Prasad <spd61185@gmail.com>
2022-04-02 05:20:00 +0530

    spring boot testing

---
+++ src/test/java/com/satishlabs/springboot/SpringbootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satishlabs.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
